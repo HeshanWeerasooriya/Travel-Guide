29ce793cea6315eabac001e92699dc37a728948e
Heshan <heshan.imedha@gmail.com>
2021-12-05 22:31:34 +0530

    cubit, cubit_state added

---
+++ lib/cubit/app_cubits.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'app_cubit_states.dart';
+
+class AppCubits extends Cubit<CubitStates> {
+  AppCubits() : super(InitialState()) {
+    emit(WelcomeState());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: DetailPage());
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DetailPage(),
+    );
   }
 }
