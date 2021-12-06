7a37af11923f33947ffc8f0fb12c1963504ded57
Heshan <heshan.imedha@gmail.com>
2021-12-06 07:34:08 +0530

    building the model data

--- lib/services/data_services.dart
+++ lib/services/data_services.dart
@@ -1,19 +1,24 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter_cubit/model/data_model.dart';
 import 'package:http/http.dart' as http;
 
 class DataServies {
   String baseUrl = "http://mark.bslmeiyu.com/api";
-  getInfo() async {
+  Future<List<DataModel>> getInfo() async {
     var apiUrl = '/getplaces';
     http.Response response = await http.get(Uri.parse(baseUrl + apiUrl));
     try {
       if (response.statusCode == 200) {
         List<dynamic> list = jsonDecode(response.body);
+        return list.map((e) => DataModel.fromJson(e)).toList();
+      } else {
+        return <DataModel>[];
       }
     } catch (e) {
       print(e);
+      return <DataModel>[];
     }
   }
 }
