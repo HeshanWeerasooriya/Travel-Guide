4748a0639ac139567d6e09e7ef91ff0f74c4e26b
Heshan <heshan.imedha@gmail.com>
2021-12-06 07:41:56 +0530

    emit() state for loading and loaded state

--- lib/cubit/app_cubits.dart
+++ lib/cubit/app_cubits.dart
@@ -6,4 +6,10 @@ class AppCubits extends Cubit<CubitStates> {
   AppCubits() : super(InitialState()) {
     emit(WelcomeState());
   }
+
+  void getData() {
+    try {
+      emit(LoadingState());
+    } catch (e) {}
+  }
 }
